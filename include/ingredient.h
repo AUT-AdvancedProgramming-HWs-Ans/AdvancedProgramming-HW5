8f1c260e5de675ab002d76c1fa7b0e231d7c0d2c
Erfan Rasti <erfanrasty@gmail.com>
2022-05-15 19:03:26 +0000

    Implementing the "Ingredient" class
    The class "Ingredient" has been implemented
    in the file "ingredient.h" file.
    The ".gitignore" file has been modified.

--- include/ingredient.h
+++ include/ingredient.h
@@ -1,4 +1,27 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
 
+// Importing the libraries
+#include <iostream>
+
+class Ingredient {
+public:
+    double get_price_unit() { return price_unit; }
+    size_t get_units() { return units; }
+    virtual std::string get_name() { return name; }
+
+    double price() { return price_unit * units; }
+
+protected:
+    Ingredient(double price_unit, size_t units)
+        : price_unit(price_unit)
+        , units(units)
+    {
+    }
+
+    double price_unit;
+    size_t units;
+    std::string name;
+};
+
 #endif // INGREDIENT_H
